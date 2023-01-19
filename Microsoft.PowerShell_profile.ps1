69384c2681c69c7e03caa53653fafa13aa841999
Leonardo Godoy <leonardoggavellar@gmail.com>
2023-01-19 04:07:18 -0300

    feat: ported aws-envs and aws-prof from bashrc to powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -18,7 +18,8 @@ Function gitconfig {
 }
 
 Function bashrc {
-  vim $PROFILE
+  vim ~/projetos/dotfiles/Microsoft.PowerShell_profile.ps1
+  cp ~/projetos/dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
   . $PROFILE
 }
 
@@ -35,10 +36,39 @@ Function wild-vault-fallback {
   $local_port=8200
   $remote_port=443
   $remote_ip='internal-vault.wildlife.io'
-  kubectl --context="${k8s_context}" --namespace="${namespace}" run "${pod_name}" --image="${image_name}" --env="LOCAL_PORT=${local_port}" --env="REMOTE_PORT=${remote_port}" --env="REMOTE_IP=${remote_ip}" --env="PROTOCOL=${protocol}"
-  kubectl --context="${k8s_context}" --namespace="${namespace}" wait --for=condition=ready pod "${pod_name}"
-  kubectl --context="${k8s_context}" --namespace="${namespace}" port-forward "${pod_name}" "${local_port}:${remote_port}"
-  kubectl --context="${k8s_context}" --namespace="${namespace}" delete pod "${pod_name}"
+  kubectl --context="${k8s_cluster}" --namespace="${namespace}" run "${pod_name}" --image="${image_name}" --env="LOCAL_PORT=${local_port}" --env="REMOTE_PORT=${remote_port}" --env="REMOTE_IP=${remote_ip}" --env="PROTOCOL=${protocol}"
+  kubectl --context="${k8s_cluster}" --namespace="${namespace}" wait --for=condition=ready pod "${pod_name}"
+  kubectl --context="${k8s_cluster}" --namespace="${namespace}" port-forward "${pod_name}" "${local_port}:${remote_port}"
+  kubectl --context="${k8s_cluster}" --namespace="${namespace}" delete pod "${pod_name}"
+}
+
+Function aws-prof {
+  if (
+    $args[0] -ceq "tfgco" -or
+    $args[0] -ceq "general-stag" -or
+    $args[0] -ceq "it-prod" -or
+    $args[0] -ceq "scalemonk" -or
+    $args[0] -ceq "playground" -or
+    $args[0] -ceq "tfgco-ufcg" -or
+    $args[0] -ceq "security-team-prod" -or
+    $args[0] -ceq "it-test" -or
+    $args[0] -ceq "wl-disaster-recovery" -or
+    $args[0] -ceq "data-dev"
+  ) {
+    aws sso login --profile $args[0]
+  }
+}
+
+Function aws-envs {
+  aws sts get-caller-identity --profile $args[0] *> $null
+  if (-not $?) {
+    aws-prof $args[0]
+  }
+  $AWS_ENVS = $(aws2-wrap.exe --profile $args[0] --export)
+  foreach ($AWS_ENV in $AWS_ENVS) {
+    Invoke-Expression $AWS_ENV
+  }
+  Set-Variable $ENV:AWS_PROFILE=$args[0]
 }
 
 # END FUNCTIONS
@@ -50,6 +80,7 @@ Set-Alias -name kns -Value kubens
 Set-Alias -name k -Value kubectl
 Set-Alias -name g -Value git
 Set-Alias -name which -Value get-command
+Set-Alias -name tf -Value terraform
 
 # END ALIASES
 
